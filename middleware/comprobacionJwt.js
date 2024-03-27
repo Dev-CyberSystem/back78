1c5d7c0768fa664f219ad39bd1b079ea2f9e7169
Diego Olmi <diegolmi@hotmail.com>
2024-03-27 08:40:35 -0300

    DB, script, admin

--- middleware/comprobacionJwt.js
+++ middleware/comprobacionJwt.js
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
 
 const comprobacionJwt = (req, res, next) => {
-    console.log(req.headers.authorization, "req")
+  console.log(req.headers.authorization, "req");
   const token = req.headers.authorization;
 
   if (!token) {
@@ -9,9 +9,18 @@ const comprobacionJwt = (req, res, next) => {
   }
 
   try {
-    const varifyToken = jwt.verify(token, process.env.SECRET_KEY);
-    req.user = varifyToken; // guardar el usuario en el req
-    next();
+    const verifyToken = jwt.verify(token, process.env.SECRET_KEY);
+    req.user = verifyToken; // guardar el usuario en el req
+
+    if (verifyToken.admin) {
+      console.log("es admin");
+      next();
+    } else {
+      console.log("no es admin");
+      return res
+        .status(401)
+        .json({ message: "Acceso denegado no es un Admin" });
+    }
   } catch (error) {
     console.log(error);
   }
